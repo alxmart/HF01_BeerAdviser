3fd4001fae1e369961cd5830acafc75609e2dcb3
alxmart <alxmartinez@gmail.com>
2024-02-27 10:44:58 -0300

    Code to select beer type and show the list.

--- app/src/main/java/com/luizafmartinez/hf01_beeradviser/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/hf01_beeradviser/MainActivity.kt
@@ -8,7 +8,7 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var findBeer : Button
+    private lateinit var findBeer: Button
     private lateinit var beerColor: Spinner
     private lateinit var brands: TextView
 
@@ -17,39 +17,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findBeer = findViewById(R.id.find_beer)
-
         findBeer.setOnClickListener {
 
             beerColor = findViewById(R.id.beer_color)
             val color = beerColor.selectedItem
 
             val beerList = getBeers(color.toString())
-            val beers =beerList.reduce { str, item -> str + '\n' + item  }
+            val beers = beerList.reduce { str, item -> str + '\n' + item }
 
             brands = findViewById(R.id.brands)
             brands.text = beers
         }
     }
 
-    fun getBeers(color: String) : List<String> {
+    fun getBeers(color: String): List<String> {
         return when (color) {
             "Light" -> listOf("Jail Pale Ale", "Lager Lite")
             "Amber" -> listOf("Jack Amber", "Red Moose")
             "Brown" -> listOf("Brown Bear Beer", "Bock Brownie")
-            else    -> listOf("Gout Stout", "Dark Daniel")
+            else -> listOf("Gout Stout", "Dark Daniel")
         }
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
 }
